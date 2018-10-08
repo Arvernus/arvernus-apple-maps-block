dac2024c97fd28bf2b232f961530cb0a566023b8
fabiankaegy <fabian@arvernus.info>
2018-10-08 08:32:27 +0200

    Working Prototype

--- blocks/maps/frontend.js
+++ blocks/maps/frontend.js
@@ -3,38 +3,80 @@
 import 'apple-mapkit-js';
 import 'apple-mapkit-js/contains';
 
+var map, mapNode;
+var workAnnotation, work, pointLongitude, pointLatitude, pointTitle, pointSubtitle, pointGlyphText, pointColor, mapType, showsMapTypeControl;
+mapkit.customDraw = function() {
+	mapType = mapNode.dataset.mapType;
+	showsMapTypeControl = mapNode.dataset.showsMapTypeControl;
+	pointLongitude = parseFloat(mapNode.dataset.pointLongitude);
+	pointLatitude = parseFloat(mapNode.dataset.pointLatitude);
+	pointTitle = mapNode.dataset.pointTitle;
+	pointSubtitle = mapNode.dataset.pointSubtitle;
+	pointGlyphText = mapNode.dataset.pointGlyphText;
+	pointColor = mapNode.dataset.pointColor;
+
+	map.mapType = mapType;
+	map.showsMapTypeControl = showsMapTypeControl;
+	// map.showsCompass = 'hidden';
+	// map.showsZoomControl = false;
+
+	if (pointLongitude && pointLatitude) {
+		work = new mapkit.Coordinate(pointLatitude, pointLongitude);
+		workAnnotation = new mapkit.MarkerAnnotation(work);
+
+		workAnnotation.color = pointColor; 
+		workAnnotation.title = pointTitle;
+		workAnnotation.subtitle = pointSubtitle;
+		workAnnotation.selected = "true";
+		workAnnotation.glyphText = pointGlyphText;    
+		map.showItems(
+			[workAnnotation],
+			{ 
+				animate: true,
+				padding: new mapkit.Padding(800, 200, 800, 200)
+			}
+		);
+	}
+}
+
+mapkit.customRedraw = function(latitude, longitude, title, subtitle, glyphText, color) {
+	workAnnotation.coordinate = new mapkit.Coordinate(parseFloat(latitude), parseFloat(longitude));
+	workAnnotation.color = color; 
+	workAnnotation.title = title;
+	workAnnotation.subtitle = subtitle;
+	workAnnotation.selected = "true";
+	workAnnotation.glyphText = glyphText;
+	map.removeItems([workAnnotation]);
+	map.showItems([workAnnotation]);
+}
+
 document.addEventListener('readystatechange', function(event) {
-    if (event.target.readyState === "interactive") {
-        mapkit.init({
-            authorizationCallback: function(done) {
-                fetch("https://arvernus.info/wp-json/AppleMapKit/v1/GetJWT/", {
-                    method: "GET",
-                    headers: {
-                        Accept: 'text/plain',
-                    },
-                },)
-                .then(function(response) {
-                    if (response.status >= 200 && response.status < 400 ) {
-                        return response.text();
-                    }
-                    else {
-                        throw `Response resulted in error ${response.status}`;
-                    }
-                })
-                .then(function(result) {
-                    done(result)
-                });
-            }
-        });
-    }
-    if (event.target.readyState === "complete") {
-
-        var mapNode = document.querySelector('#map');
-
-        var map = new mapkit.Map(mapNode);
-        map.mapType = mapkit.Map.MapTypes.Hybrid;
-        map.showsMapTypeControl = false;
-        map.showsCompass = 'hidden';
-        map.showsZoomControl = false;
-    }
+	if (event.target.readyState === "interactive") {
+		mapkit.init({
+			authorizationCallback: function(done) {
+				fetch("https://arvernus.info/wp-json/AppleMapKit/v1/GetJWT/", {
+					method: "GET",
+					headers: {
+						Accept: 'text/plain',
+					},
+				},)
+				.then(function(response) {
+					if (response.status >= 200 && response.status < 400 ) {
+						return response.text();
+					}
+					else {
+						throw `Response resulted in error ${response.status}`;
+					}
+				})
+				.then(function(result) {
+					done(result)
+				});
+			}
+		});
+	}
+	if (event.target.readyState === "complete") {
+		mapNode = document.querySelector('#map');
+		map = new mapkit.Map(mapNode);
+		mapkit.customDraw();
+	}
 }, false );
--- blocks/maps/index.js
+++ blocks/maps/index.js
@@ -4,25 +4,31 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import { mapkit, contains } from 'apple-mapkit-js';
 
 /**
  * WordPress dependencies
  */
-const { __, sprintf } = wp.i18n;
-const { PanelBody, RangeControl } = wp.components;
-const { Fragment } = wp.element;
+const { 
+	__, 
+	sprintf 
+} = wp.i18n;
+const { 
+	Fragment,
+} = wp.element;
+const {
+	PanelBody,
+	TextControl,
+	ToggleControl,
+	SelectControl,
+	Button,
+} = wp.components;
 const {
   registerBlockType,
-  Editable,
-  BlockControls,
-  InspectorControls,
-  BlockDescription,
-  AlignmentToolbar,
-  BlockAlignmentToolbar,
 } = wp.blocks;
 const {
-	InnerBlocks,
+	InspectorControls,
+	withColors,
+	PanelColorSettings,
 } = wp.editor;
 
 /**
@@ -31,34 +37,180 @@ const {
 import './style.scss';
 import './editor.scss';
 
+const mapAttributes = {
+	mapType: {
+		type: 'string',	
+		default: mapkit.Map.MapTypes.Standard,
+	},
+	searchQuery: {
+		type: 'string',
+	},
+	showsMapTypeControl: {
+		type: 'Boolean',
+		default: true,
+	},
+	pointLatitude: {
+		type: 'number',
+		default: '37.334852',
+	},
+	pointLongitude: {
+		type: 'number',
+		default: '-122.009163',
+	},
+	pointTitle: {
+		type: 'string',
+		default: 'Title',
+	},
+	pointSubtitle: {
+		type: 'string',
+		default: 'Subtitle',
+	},
+	pointGlyphText: {
+		type: 'string',
+		default: 'T',
+	},
+	pointColor: {
+		type: 'string',
+		default: '#000000',
+	}
+};
+
+const mapTypeOptions = [
+	{ value: mapkit.Map.MapTypes.Satellite, label: __( 'Satellite' ) },
+	{ value: mapkit.Map.MapTypes.Hybrid, label: __( 'Hybrid' ) },
+	{ value: mapkit.Map.MapTypes.Standard, label: __( 'Standard' ) }
+];
+
 registerBlockType(
 	'mapkitjs/map', {
 		title: 'Mapkit',
 		icon: {
-			background: '#39ab00',
-		    foreground: '#fff',
 			src: 'location-alt',
 		},
-		description: 'Slider for Clients.',
+		description: 'Apple Maps',
 		category: 'common',
+		attributes: mapAttributes,
 		supports: {
 			align: [ 'wide', 'full' ],
 		},
-
 		edit: props => {
+			const { attributes: { showsMapTypeControl, pointLatitude, pointLongitude, pointTitle, pointSubtitle, pointGlyphText, pointColor, mapType, searchQuery }, className, setAttributes } = props;
+			const redrawMap = () => {
+				console.log('redraw');
+				mapkit.customRedraw(pointLatitude, pointLongitude, pointTitle, pointSubtitle, pointGlyphText, pointColor);
+			};
+			const toggleMapTypeControl = ( value ) => {
+				setAttributes( {showsMapTypeControl: !showsMapTypeControl } );
+			};
+			const startSearch = () => {
+				let search = new mapkit.Search();
+				if (searchQuery) {
+					search.search(searchQuery, (error, data) => {
+						if (error) {
+							return;
+						}
+						setAttributes( { pointLatitude: data.places[0].coordinate.latitude } );
+						setAttributes( { pointLongitude: data.places[0].coordinate.longitude } );
+						redrawMap();
+					} )
+				}
+			};
 			return (
 				<Fragment>
-					<div className={ props.className } id="map">
-						<h2>{ __( 'Mapkit' ) }</h2>
-					</div>
+					<InspectorControls>
+						<PanelBody title={ __( 'Map Settings' ) }>
+							<ToggleControl
+								label={__( 'Show Map Type Control' ) }
+								help={ showsMapTypeControl ? __('Map Type Control is visible.') : __('Map Type Control is hidden.') } 
+								checked={ showsMapTypeControl }
+								onChange={ toggleMapTypeControl  }
+							/>
+							<SelectControl
+								label={ __( 'Map Type' ) }
+								value={ mapType }
+								onChange={ (value) => { setAttributes( { mapType: value } ); redrawMap() } }
+								options={ mapTypeOptions }
+							/>
+						</PanelBody>
+						<PanelBody title={ __( 'Location Settings' ) }>
+							<TextControl
+								label={ __( 'Titel' ) }
+								value={ pointTitle }
+								onChange={ (value) => { setAttributes( { pointTitle: value } ); redrawMap() } }
+							/>
+							<TextControl
+								label={ __( 'Subtitle' ) }
+								value={ pointSubtitle }
+								onChange={ (value) => { setAttributes( { pointSubtitle: value } ); redrawMap() } }
+							/>
+							<TextControl
+								label={ __( 'Glyph Text' ) }
+								value={ pointGlyphText }
+								onChange={ (value) => { setAttributes( { pointGlyphText: value } ); redrawMap() } }
+							/>
+							<PanelColorSettings
+								title={ __( 'Color Settings' ) }
+								colorSettings={ [
+									{
+										value: pointColor,
+										onChange: (value) => { setAttributes( { pointColor: value } ); redrawMap() },
+										label: __( 'Glyph Color' ),
+									},
+								] }
+							/>
+							<TextControl
+								label={ __( 'Search Place' ) }
+								value={ searchQuery }
+								onChange={ (value) => { setAttributes( { searchQuery: value } ) } }
+							/>
+							<Button isDefault
+								onClick={ startSearch }>
+								Search
+							</Button>
+							<TextControl
+								label={ __( 'Latitude' ) }
+								value={ pointLatitude }
+								onChange={ (value) => { setAttributes( { pointLatitude: value } ); redrawMap() } }
+							/>
+							<TextControl
+								label={ __( 'Longitude' ) }
+								value={ pointLongitude }
+								onChange={ (value) => { setAttributes( { pointLongitude: value } ); redrawMap() } }
+							/>
+						</PanelBody>
+					</InspectorControls>
+					<div 
+						className={ props.className }
+						id="map"
+						data-shows-map-type-control={ showsMapTypeControl }
+						data-map-type={ mapType }
+						data-point-title={ pointTitle }
+						data-point-subtitle={ pointSubtitle }
+						data-point-glyph-text={ pointGlyphText }
+						data-point-latitude={ pointLatitude }
+						data-point-longitude={ pointLongitude }
+						data-point-color={ pointColor }
+					></div>
 				</Fragment>
 			);
+
 		}, // end edit
 
 		save: props => {
+			const { attributes: { showsMapTypeControl, pointLatitude, pointLongitude, pointTitle, pointSubtitle, pointGlyphText, pointColor, mapType }, className, setAttributes } = props;
 			return (
-				<div className={ props.className } id="map">
-				</div>
+				<div 
+					className={ props.className }
+					id="map"
+					data-shows-map-type-control={ showsMapTypeControl }
+					data-map-type={ mapType }
+					data-point-title={ pointTitle }
+					data-point-subtitle={ pointSubtitle }
+					data-point-glyph-text={ pointGlyphText }
+					data-point-latitude={ pointLatitude }
+					data-point-longitude={ pointLongitude }
+					data-point-color={ pointColor }
+				></div>
 			);
 		},
 } );
