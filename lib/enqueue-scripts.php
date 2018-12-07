10377f98215dba6fcd5b8b41e76752968bb4e591
fabiankaegy <fabian@arvernus.info>
2018-12-07 20:03:16 +0100

    wp5.0 ready

--- lib/enqueue-scripts.php
+++ lib/enqueue-scripts.php
@@ -13,7 +13,7 @@ function enqueue_block_editor_assets() {
 
 	// Enqueue the bundled block JS file
 	wp_enqueue_script(
-		'jsforwp-blocks-js',
+		'arvernus-apple-maps-blocks-js',
 		_get_plugin_url() . $block_path,
 		[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
 		filemtime( _get_plugin_directory() . $block_path )
@@ -21,7 +21,7 @@ function enqueue_block_editor_assets() {
 
 	// Enqueue optional editor only styles
 	wp_enqueue_style(
-		'jsforwp-blocks-editor-css',
+		'arvernus-apple-maps-blocks-editor-css',
 		_get_plugin_url() . $style_path,
 		[ 'wp-blocks' ],
 		filemtime( _get_plugin_directory() . $style_path )
@@ -35,9 +35,9 @@ add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_assets' );
 function enqueue_assets() {
 	$style_path = '/assets/css/blocks.style.css';
 	wp_enqueue_style(
-		'jsforwp-blocks',
+		'arvernus-apple-maps-blocks',
 		_get_plugin_url() . $style_path,
-		[ 'wp-blocks' ],
+		null,
 		filemtime( _get_plugin_directory() . $style_path )
 	);
 }
@@ -55,7 +55,7 @@ function enqueue_frontend_assets() {
 
 	$block_path = '/assets/js/frontend.blocks.js';
 	wp_enqueue_script(
-		'jsforwp-blocks-frontend',
+		'arvernus-apple-maps-blocks-frontend',
 		_get_plugin_url() . $block_path,
 		[ 'wp-dom-ready' ],
 		filemtime( _get_plugin_directory() . $block_path )
